7fd0bd89b9a094134c58be8cf8a295db9acae235
Glenn Griffin <ggriffiniii@gmail.com>
2021-06-07 22:48:01 -0700

    Add support for converting to arbitrary field types.
    The target type of a field may now be something other than unsinged
    integers or bools. For any type other than unsigned int or bool the
    traits FromRaw and IntoRaw will need to be implemented for the bitstruct
    the field is defined on.

--- bitstruct/src/lib.rs
+++ bitstruct/src/lib.rs
@@ -1,1 +1,39 @@
 pub use bitstruct_derive::bitstruct;
+
+pub trait FromRaw<Raw, Target> {
+    fn from_raw(raw: Raw) -> Target;
+}
+
+pub trait IntoRaw<Raw, Target> {
+    fn into_raw(target: Target) -> Raw;
+}
+
+/// Blanket impl of FromRaw for any type that has Into<Target> implemented.
+/// This allows types that have universal conversions to define Into<Target>
+/// rather than a conversion per bitfield. If the Target type does not have a
+/// universal representation (i.e. it varies depending on the bitstruct) you
+/// should instead implement FromRaw for each particular bitstruct field that
+/// contains the Target type.
+impl<T, Raw, Target> FromRaw<Raw, Target> for T
+where
+    Raw: Into<Target>,
+{
+    fn from_raw(raw: Raw) -> Target {
+        raw.into()
+    }
+}
+
+/// Blanket impl of IntoRaw for any type that has Into<Raw> implemented.
+/// This allows types that have universal conversions to define Into<Raw>
+/// rather than a conversion per bitfield. If the Target type does not have a
+/// universal representation (i.e. it varies depending on the bitstruct) you
+/// should instead implement IntoRaw for each particular bitstruct field that
+/// contains the Target type.
+impl<T, Raw, Target> IntoRaw<Raw, Target> for T
+where
+    Target: Into<Raw>,
+{
+    fn into_raw(target: Target) -> Raw {
+        target.into()
+    }
+}
